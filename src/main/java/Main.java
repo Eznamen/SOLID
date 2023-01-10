6b9804d9e4ae261b2e0281798775a63927c50ab4
Eznamen <111395043+Eznamen@users.noreply.github.com>
2023-01-11 01:37:12 +0300

    changed the logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,30 +3,27 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Products products;
+        Map<String, Integer> products = new HashMap<>();
+        Stock.addProd(new Product("Bread", 56), products);
+        Stock.addProd(new Product("Sausage", 211), products);
+        Stock.addProd(new Product("Oil", 153), products);
+        Stock.addProd(new Product("Pie", 45), products);
         Purchase purchase;
-
-        HashMap<String, Integer> productses = new HashMap<>();
-        products = new Products(productses, "Bread", 56);
-        products = new Products(productses, "Sausage", 211);
-        products = new Products(productses, "Oil", 153);
-        products = new Products(productses, "Pie", 45);
-        Products.toStringProducts(productses);
-        Map<String, Integer> map = new HashMap<>();
+        Basket basket = new Basket();
+        Stock.toStringProducts(products);
 
         while (true) {
             System.out.println("Enter two words: product name and count. Or end");
             Scanner scanner = new Scanner(System.in);
-            purchase = new Purchase();
-            String line = scanner.nextLine();
+
+            String line = scanner.nextLine().toLowerCase();
             if ("end".equals(line)) break;
             String[] parts = line.split(" ");
-            String product = parts[0];
+            String title = parts[0];
             int count = Integer.parseInt(parts[1]);
-            map = purchase.addPurchase(product, count, map);
+            purchase = new Purchase(title, count);
+            basket.addPurchase(purchase);
         }
-        long sum = purchase.sum(productses, map);
-
-        purchase.toStringSum(sum, productses, map);
+        basket.toStringProducts(products);
     }
 }
--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -1,39 +1,19 @@
-import java.util.Map;
 
 public class Purchase {
-    private long sum = 0;
+    int count;
+    String title;
 
-    public Purchase() {
+    public Purchase(String title, int count) {
+        this.title = title.toLowerCase();
+        this.count = count;
     }
 
-    public Map<String, Integer> addPurchase(String title, int count, Map<String, Integer> map) {
-        if (map.get(title) == null) {
-            map.put(title, count);
-        } else {
-            if (map.containsKey(title)) {
-                int ccount = map.get(title) + count;
-                map.put(title, ccount);
-            }
-        }
-        return map;
+    public int getCount() {
+        return count;
     }
 
-    public long sum(Map<String, Integer> prices, Map<String, Integer> map) {
-        for (Map.Entry<String, Integer> entry : prices.entrySet()) {
-            if (map.containsKey(entry.getKey())) {
-                sum = entry.getValue() * map.get(entry.getKey()) + sum;
-            } else {
-                continue;
-            }
-        }
-        return sum;
+    public String getTitle() {
+        return title;
     }
 
-    public void toStringSum(long sum, Map<String, Integer> prices, Map<String, Integer> map) {
-        System.out.println("Shopping cart:");
-        System.out.println(sum + " $");
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println("\t" + entry.getKey() + " " + entry.getValue() + " unit by price " + (entry.getValue() * prices.get(entry.getKey()) + " $"));
-        }
-    }
 }
